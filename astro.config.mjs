698133101ddc56a7ba69cceb0456ddcad47ffe91
Jesus Zermeño <josezerrod@hotmail.com>
2024-02-07 13:35:06 -0600

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
+import partytown from "@astrojs/partytown";
+import react from '@astrojs/react';
+import tailwind from "@astrojs/tailwind";
+import astroI18next from "astro-i18next";
+
+
+// https://astro.build/config
+export default defineConfig({
+    site: "https://hfmexico.mx/americas-mobility-future/",
+    base: "/americas-mobility-future",  
+  integrations: [sitemap({
+    i18n: {
+      defaultLocale: "es",
+      locales: {
+        en: "en",
+        es: "es"
+      }
+    }
+  }), partytown(), tailwind(), react(), astroI18next()]
+});
