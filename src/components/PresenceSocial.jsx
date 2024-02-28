c2d9e721018b190e1b455696a6232f88faaac9bf
Pachi-Dev <pachi.claros@gmail.com>
2024-02-28 16:19:42 -0600

    Agregadas imágenes y archivos relacionados con el toolkit. Añadido hook useNearScreen y componente Bullets. Actualizado package.json y package-lock.json.

---
+++ src/components/PresenceSocial.jsx
@@ -0,0 +1,43 @@
+import { useNearScreen } from '../hooks/useNearScreen'
+import { Bullets } from './Bullets'
+
+export function PresenceSocial({
+  title,
+  followers,
+  network,
+  people,
+  reach,
+  coverage,
+  allsocial,
+}) {
+  const [show, ref] = useNearScreen()
+  return (
+    <div ref={ref}>
+      {show && (
+        <>
+          <p
+            className='text-white font-bold text-4xl py-4 px-20 mt-20 ms-[-8px] uppercase'
+            style={{
+              backgroundImage:
+                'linear-gradient(135deg, rgb(3, 40, 126), rgb(0, 110, 234) 50%, rgb(96, 226, 220) 100%)',
+            }}
+          >
+            {title}
+          </p>
+          <div className='text-center uppercase'>
+            <Bullets number='21000' duration='4' />
+            <p className='text-gray-600 font-extrabold text-4xl'>{followers}</p>
+            <p className='text-gray-600 text-xl'>{network}</p>
+            <Bullets number='2500000' duration='4' />
+            <p className='text-gray-600 font-extrabold text-4xl'>{people}</p>
+            <p className='text-gray-600 text-xl'>{reach}</p>
+            <p className='mt-10 text-gray-600 font-extrabold text-4xl'>
+              {coverage}
+            </p>
+            <p className='text-gray-600 text-xl'>{allsocial}</p>
+          </div>
+        </>
+      )}
+    </div>
+  )
+}
---
+++ src/constants_toolkit.js
@@ -0,0 +1,17 @@
+const toolkit_es = [
+    { id: 0, src: '/img/toolkit_banner_website_es.webp', alt:'Banner web', title:'Banner web', width: 360, height: 225 },
+    { id: 1, src: '/img/toolkit_key_visual_es.webp', alt:'KeyVisual', title:'KeyVisual', width: 360, height: 158 },
+    { id: 2, src: '/img/toolkit_facebook_es.webp', alt:'Facebook', title:'Facebook', width: 360, height: 158 },
+    { id: 3, src: '/img/toolkit_twitter_es.webp', alt:'Twitter', title:'Twitter', width: 360, height: 120 },
+    { id: 4, src: '/img/toolkit_linkedin_es.webp', alt:'Linkedin', title:'Linkedin', width: 360, height: 90 }    
+]
+
+const toolkit_en = [
+    { id: 0, src: '/img/toolkit_banner_website_en.webp', alt:'Banner web', title:'Banner web', width: 360, height: 225 },
+    { id: 1, src: '/img/toolkit_key_visual_en.webp', alt:'KeyVisual', title:'KeyVisual', width: 360, height: 158 },
+    { id: 2, src: '/img/toolkit_facebook_en.webp', alt:'Facebook', title:'Facebook', width: 360, height: 158 },
+    { id: 3, src: '/img/toolkit_twitter_en.webp', alt:'Twitter', title:'Twitter', width: 360, height: 120 },
+    { id: 4, src: '/img/toolkit_linkedin_en.webp', alt:'Linkedin', title:'Linkedin', width: 360, height: 90 }    
+]
+
+export {toolkit_es, toolkit_en}
