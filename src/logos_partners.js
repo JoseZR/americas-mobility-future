a7427deb772f495d693f07cf8eb92d51a218106e
Pachi-Dev <pachi.claros@gmail.com>
2024-02-29 17:54:30 -0600

    Actualizar lista de socios en logos_partners.js

--- src/logos_partners.js
+++ src/logos_partners.js
@@ -7,8 +7,7 @@ const partners = [
     { link: '', src: '/img/logos/logistic-delivery.webp', alt:'', width: 200, height: 190 },
     { link: '', src: '/img/logos/dpl-news.webp', alt:'', width: 200, height: 190 },
     { link: '', src: '/img/logos/global-industries.webp', alt:'', width: 200, height: 190 },
-    { link: '', src: '/img/logos/consultoria.webp', alt:'', width: 200, height: 190 },
-    { link: '', src: '/img/logos/metropoli-mid.webp', alt:'', width: 200, height: 190 }
+    { link: '', src: '/img/logos/consultoria.webp', alt:'', width: 200, height: 190 },    
 
 ]
 export {partners}
