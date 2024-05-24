3f0b41503b4b9af4dfe16d108f8a350ace622364
Pachi-Dev <pachi.claros@gmail.com>
2024-05-24 11:16:19 -0600

    Actualizar nombre de imagen y corregir errores de escritura en traducción

--- src/components/PresenceSocial.jsx
+++ src/components/PresenceSocial.jsx
@@ -1,4 +1,3 @@
-import { useNearScreen } from '../hooks/useNearScreen'
 import { Bullets } from './Bullets'
 
 export function PresenceSocial({
@@ -10,34 +9,31 @@ export function PresenceSocial({
   coverage,
   allsocial,
 }) {
-  const [show, ref] = useNearScreen()
   return (
-    <div ref={ref}>
-      {show && (
-        <>
-          <p
-            className='text-white font-bold text-4xl py-4 px-20 mt-20 ms-[-8px] uppercase'
-            style={{
-              backgroundImage:
-                'linear-gradient(135deg, rgb(3, 40, 126), rgb(0, 110, 234) 50%, rgb(96, 226, 220) 100%)',
-            }}
-          >
-            {title}
+    <div>
+      <>
+        <p
+          className='text-white font-bold text-4xl py-4 px-20 mt-20 ms-[-8px] uppercase'
+          style={{
+            backgroundImage:
+              'linear-gradient(135deg, rgb(3, 40, 126), rgb(0, 110, 234) 50%, rgb(96, 226, 220) 100%)',
+          }}
+        >
+          {title}
+        </p>
+        <div className='text-center uppercase'>
+          <Bullets number='6135' duration='4' />
+          <p className='text-gray-600 font-extrabold text-4xl'>{followers}</p>
+          <p className='text-gray-600 text-xl'>{network}</p>
+          <Bullets number='4200000' duration='4' />
+          <p className='text-gray-600 font-extrabold text-4xl'>{people}</p>
+          <p className='text-gray-600 text-xl'>{reach}</p>
+          <p className='mt-10 text-gray-600 font-extrabold text-4xl'>
+            {coverage}
           </p>
-          <div className='text-center uppercase'>
-            <Bullets number='6135' duration='4' />
-            <p className='text-gray-600 font-extrabold text-4xl'>{followers}</p>
-            <p className='text-gray-600 text-xl'>{network}</p>
-            <Bullets number='4200000' duration='4' />
-            <p className='text-gray-600 font-extrabold text-4xl'>{people}</p>
-            <p className='text-gray-600 text-xl'>{reach}</p>
-            <p className='mt-10 text-gray-600 font-extrabold text-4xl'>
-              {coverage}
-            </p>
-            <p className='text-gray-600 text-xl'>{allsocial}</p>
-          </div>
-        </>
-      )}
+          <p className='text-gray-600 text-xl'>{allsocial}</p>
+        </div>
+      </>
     </div>
   )
 }
