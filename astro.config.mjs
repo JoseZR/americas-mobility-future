3668d53d498a21748b9e41ec03a30139c1ebad65
Pachi-Dev <pachi.claros@gmail.com>
2024-07-02 15:09:34 -0600

    chore: Update i18n configuration and remove unused code

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://americas-mobility-future/",
+  site: "https://americas-mobility-future",
   integrations: [sitemap({
     i18n: {
       defaultLocale: "es",
