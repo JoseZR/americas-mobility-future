7da2f279a5e9da90840ae06bd6b3a4b2912fcd68
Pachi-Dev <pachi.claros@gmail.com>
2024-07-16 16:51:46 -0600

    feat: Update download access link to use production URL

--- src/components/register/DonwloadAccess.jsx
+++ src/components/register/DonwloadAccess.jsx
@@ -5,7 +5,7 @@ export function DonwloadAccess({ text }) {
   return (
     <>
       <a
-        href={`/invoices/${invoiceDownToLoad}`}
+        href={`https://industrialtransformation.mx/invoices/${invoiceDownToLoad}`}
         target='_blank'
         className='mt-10 text-white px-4 py-2 rounded-md font-bold bg-[#03CAFE] hover:bg-[#236d7f] transition duration-300 ease-in-out'
       >
