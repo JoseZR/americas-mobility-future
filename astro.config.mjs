61b6c36e9d13c270ac270a912ea6d52d26f65bb8
Pachi-Dev <pachi.claros@gmail.com>
2024-07-18 11:55:13 -0600

    chore: Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://americas-mobility-future",
+  site: "https://amofexpo.igeco.mx",
   integrations: [tailwind(), react(), astroI18next(),sitemap({
     i18n: {
       defaultLocale: "es",
