bed59527feca798140431f302a48adf399a46e8a
Pachi-Dev <pachi.claros@gmail.com>
2025-04-01 17:39:04 -0600

    agrega fondo y sección de héroe para AMOF 2025, actualiza configuración y estilos globales

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,9 @@ import astroI18next from "astro-i18next";
 // https://astro.build/config
 export default defineConfig({
   site: "https://amofexpo.igeco.mx",
-  integrations: [tailwind(), react(), astroI18next(),sitemap({
+  integrations: [tailwind({
+    applyBaseStyles: false,
+  }), react(), astroI18next(),sitemap({
     i18n: {
       defaultLocale: "es",
       locales: {
